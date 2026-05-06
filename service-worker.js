b0e78b6088080539bb0801266ab080c5072dc4e6
Alan Lima <delima20k@gmail.com>
2026-05-06 12:20:03 -0300

    fix(chat): cards de membros em 3 colunas fixas

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'milionarios-v3.8';
-const STATIC_CACHE = 'milionarios-static-v3.8';
-const DYNAMIC_CACHE = 'milionarios-dynamic-v3.8';
+const CACHE_NAME = 'milionarios-v3.9';
+const STATIC_CACHE = 'milionarios-static-v3.9';
+const DYNAMIC_CACHE = 'milionarios-dynamic-v3.9';
 
 // Recursos essenciais para cache
 const CORE_ASSETS = [
